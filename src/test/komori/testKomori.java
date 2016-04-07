30248c5730bc7fd547126cf238504a6a5bb89a4e
KKomor <kenichiro.komori@hcrisis2.onmicrosoft.com>
2016-04-07 16:18:51 +0900

    javaテスト
    javaテストです

---
+++ src/test/komori/testKomori.java
@@ -0,0 +1,10 @@
+package test.komori;
+
+/**
+ * Created by komori on 2016/04/07.
+ */
+public class testKomori {
+    public static void main(String args[]) {
+        System.out.println("hellooo");
+    }
+}
